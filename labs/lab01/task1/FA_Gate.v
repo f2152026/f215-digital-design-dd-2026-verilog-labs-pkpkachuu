f78f5f7166b5f65896e64b61f4a0be4f609b4c77
Pahulpreet Kaur <192534806+pkpkachuu@users.noreply.github.com>
2026-08-26 16:03:00 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -8,17 +8,21 @@
 //           re-simulate with the same tb.v and compare.
 
 module FA_Gate(
-  input  a,
-  input  b,
-  input  cin,
+  input a,
+  input b,
+  input cin,
   output sum,
   output cout
 );
+
   wire ps, pc1, pc2;
-  and (pc1, a,   b);
-  or  (cout, pc1, pc2);
-  xor (ps,  a,   b);
+
   xor (sum, cin, ps);
   and (pc2, cin, ps);
+  xor (ps, a, b);
+  and (pc1, a, b);
+  or  (cout, pc1, pc2);
 
 endmodule
+
+
