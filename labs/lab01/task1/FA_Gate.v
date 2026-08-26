4e0b7531878b9ee71f167b9113f8425932916dcd
Pahulpreet Kaur <192534806+pkpkachuu@users.noreply.github.com>
2026-08-26 16:01:39 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -15,11 +15,10 @@ module FA_Gate(
   output cout
 );
   wire ps, pc1, pc2;
-
+  and (pc1, a,   b);
   or  (cout, pc1, pc2);
   xor (ps,  a,   b);
   xor (sum, cin, ps);
   and (pc2, cin, ps);
-  and (pc1, a,   b);
 
 endmodule
